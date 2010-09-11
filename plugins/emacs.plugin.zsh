b648537495569f5797128e9413d4da1d3df5dc5c
Yves Senn <yves.senn@gmail.com>
2010-09-11 11:30:23 +0200

    . use emacs installed by homebrew

--- plugins/emacs.plugin.zsh
+++ plugins/emacs.plugin.zsh
@@ -1,4 +1,4 @@
-alias ec='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
+alias ec='/usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/bin/emacsclient'
 alias e=openemacs
 
 
@@ -6,6 +6,6 @@ function openemacs {
     if [ -f ~/.emacsserver ]; then
         ec $1
     else
-        open -a emacs $1
+        open -a /usr/local/Cellar/emacs/HEAD/Emacs.app $1
     fi
 }
--- plugins/organization.plugin.zsh
+++ plugins/organization.plugin.zsh
@@ -6,12 +6,10 @@ function c () {
   cd ~/Projects/$1;
 }
 
-#compdef h
 function _h () {
   _files -W ~ -/
 }
 
-#compdef c
 function _c () {
   _files -W ~/Projects -/
 }

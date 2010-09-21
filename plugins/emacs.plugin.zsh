0836bdf64abc0f2f1894a61d4a63e0666e853aac
Yves Senn <yves.senn@gmail.com>
2010-09-21 11:05:01 +0200

    . use emacs installed with homebrew

--- plugins/emacs.plugin.zsh
+++ plugins/emacs.plugin.zsh
@@ -4,10 +4,14 @@ alias e=openemacs
 EDITOR='openemacs'
 export EDITOR
 
+# configuration for emacs multi-term.el compatibility
+bindkey "\ef"    forward-word
+bindkey "\eb"    backward-word
+
 function openemacs {
     if [ -f ~/.emacsserver ]; then
         ec $1
     else
-        open -a /usr/local/Cellar/emacs/HEAD/Emacs.app $1
+        open -a /usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/Emacs $1
     fi
 }

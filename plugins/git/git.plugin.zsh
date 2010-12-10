6a880d806d8836f601361630a37db9295295ead7
Yves Senn <yves.senn@gmail.com>
2010-12-10 17:13:23 +0100

    . more git alias awesomeness

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,27 +1,24 @@
 # Aliases
 alias g='git'
 alias gm='git merge'
+alias gmf='git merge --ff-only'
 alias gs='git status'
 alias gl='git pull'
 alias glr='git pull --rebase'
 alias gf='git fetch'
 alias gp='git push'
 alias gd='git diff'
-alias gdv='git diff -w "$@" | vim -R -'
 alias gc='git commit -v'
 alias gca='git commit -v -a'
-alias gb='git branch'
-alias gba='git branch -a'
+alias gb='git branch -v'
+alias gba='git branch -a -v'
 alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 alias glg='git log --stat --max-count=5'
-alias gg='git log'
+alias gg='git log --graph'
 alias gco='git checkout'
 alias gsv='git svn'
-alias gt='git stash'
-
-# Git and svn mix
-alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
+alias gt='git tag'
 
 #
 # Will return the current branch name

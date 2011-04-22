78285f8a50accfa856609494450a4783d6ebef68
Yves Senn <yves.senn@gmail.com>
2011-04-22 21:13:00 +0200

    TorqueBox Plugin

---
+++ plugins/torquebox/torquebox.plugin.zsh
@@ -0,0 +1,5 @@
+export TORQUEBOX_HOME=$HOME/Projects/software/torquebox-current
+export JBOSS_HOME=$TORQUEBOX_HOME/jboss
+export JRUBY_HOME=$TORQUEBOX_HOME/jruby
+
+export PATH=$JRUBY_HOME/bin:$PATH

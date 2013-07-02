f1f8097472f0a0f2f62764d13fcef3d63780f135
Yves Senn <yves.senn@gmail.com>
2013-07-02 10:04:44 +0200

    don't overwrite `gm` alias.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -72,8 +72,8 @@ alias gss='git status -s'
 compdef _git gss=git-status
 alias ga='git add'
 compdef _git ga=git-add
-alias gm='git merge'
-compdef _git gm=git-merge
+# alias gm='git merge'
+# compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
 

bd81cebc8645bd4603ce654a94ee635b7ab71f65
Yves Senn <yves.senn@gmail.com>
2015-01-26 17:08:42 +0100

    let's just stick to vanilla `emacsclient`.

--- plugins/emacs/emacs.plugin.zsh
+++ plugins/emacs/emacs.plugin.zsh
@@ -1,17 +1,5 @@
-alias ec='/usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/bin/emacsclient'
-alias e=openemacs
+alias ec='/usr/local/bin/emacsclient'
+alias e='/usr/local/bin/emacsclient -n' -n
 
-EDITOR='/usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/bin/emacsclient'
+EDITOR='/usr/local/bin/emacsclient'
 export EDITOR
-
-# configuration for emacs multi-term.el compatibility
-bindkey "\ef"    forward-word
-bindkey "\eb"    backward-word
-
-function openemacs {
-    if [ -f ~/.emacsserver ]; then
-        ec $1
-    else
-        open -a /usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/Emacs $1
-    fi
-}
